bc4ee94ee09248d3d745430e695f3a2c09330457
Merith <merith.tk@gmail.com>
2021-12-11 06:03:08 +0000

    base repo setup

---
+++ host.exec.cpp
@@ -0,0 +1,39 @@
+extern "C" {
+	#include <lua.h>
+	#include <lauxlib.h>
+	#include <stdlib.h>
+}
+#include <CraftOS-PC.hpp>
+
+// add your functions here...
+
+int execute(lua_State *L) {
+	const char * str = lua_tostring(L, 1);
+	system(str);
+	return(0)
+}
+
+static luaL_reg M[] = {
+    {"exec", execute},
+	{NULL,NULL}
+};
+
+static PluginInfo info("host");
+
+extern "C" {
+#ifdef _WIN32
+_declspec(dllexport)
+#endif
+
+int luaopen_host(lua_State *L) {
+    luaL_register(L, "host", M);
+    return 1;
+}
+
+#ifdef _WIN32
+_declspec(dllexport)
+#endif
+PluginInfo * plugin_init(PluginFunctions * func, const path_t& path) {
+    return &info;
+}
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+default:
+	cpp host.exec.cpp -Icraftos2/api -Icraftos2/craftos2-lua/include
+	./a.out
